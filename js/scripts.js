a1422efdff5491239d9ca2cd67675fe6265343a3
Usuario Local <UsuarioLocal@EQUIP003034.xunta.local>
2021-11-02 09:56:48 +0100

    Ejercicio iniciado de js
    -Ejercicio 1,2,3,4 de los encargados al alumno

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+"use strict";
+//exercicio1
+
+//let nombrePila = "Xurxo"; 
+//console.log(nombrePila); 
+
+//exercicio2
+
+//nombrePila = "Jorge";
+//console.log(nombrePila);
+
+//exercicio3
+//se comento variábel dime que non está definida
+
+//exercicio4
+
+let operador1=3; 
+let operador2=5, operador3="Xurxo";
+let resultado=0;
+
+resultado = operador1+operador2;
+
+console.log(operador1+operador2)
+
+
+//quero ver outra vez o resultado
+operador1 = 67;
+
+console.log(operador1+operador2);
+
+//quero ver o 8
+console.log(resultado);
+
+
+
+//let operador1=1, operador2=2, operador3=3;
+//resultado = operador1+operador2;
+//console.log(resultado);
